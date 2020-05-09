29b206c24d8d1b3da68d639f913fa5566e2e8a09
unknown <mhamad.aa1997.aa@gmail.com>
2020-05-09 18:11:46 +0300

    done test

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -1,15 +1,17 @@
-import { request } from 'graphql-request';
-import { startServer } from 'src/startServer';
-import { User } from 'src/entity/user';
-import { ERROR_MESSAGE_EMAIL } from '../user/Register/ErrorMessages';
+import { request } from "graphql-request";
+import { startServer } from "src/startServer";
+import { User } from "src/entity/user";
+import {
+  ERROR_MESSAGE_EMAIL,
+  ERROR_MESSAGE_PASSWORD,
+} from "../user/Register/ErrorMessages";
 enum UserTest {
-     email="test@test.com",
-     password="test@test"
-
+  email = "test@test.com",
+  password = "test@test",
 }
-let getHost = ()=> ""
+let getHost = () => "";
 
-const mutation=(email:string,password:string)=>`
+const mutation = (email: string, password: string) => `
 mutation {
      register(data:{email:"${email}",password:${password}}){
           path
@@ -17,58 +19,63 @@ mutation {
 
      }
 }
-`
-beforeAll(async()=>{
-
-    const app= await startServer();
-    const { port }:any = app.address();
-    getHost=()=>`http://127.0.0.1:${port}`
-
-})
+`;
+beforeAll(async () => {
+  const app = await startServer();
+  const { port }: any = app.address();
+  getHost = () => `http://127.0.0.1:${port}`;
+});
 // WE NEED TO DROP OF ALL THE DATABASE BEFORE START
-test('should register user', async() => {
-     const res=await request(getHost(),mutation("test@test.com","test@test.com"  ));
-     expect(res).toEqual({register:null});
-     const users:User[]=await User.find({where:{email:UserTest.email}})
-     expect(users).toHaveLength(1);
-     expect(users[0].email).toEqual(UserTest.email)
-     expect(users[0].password).not.toEqual(UserTest.password);
+test("should register user", async () => {
+  const res = await request(
+    getHost(),
+    mutation("test@test.com", "test@test.com")
+  );
+  expect(res).toEqual({ register: null });
+  const users: User[] = await User.find({ where: { email: UserTest.email } });
+  expect(users).toHaveLength(1);
+  expect(users[0].email).toEqual(UserTest.email);
+  expect(users[0].password).not.toEqual(UserTest.password);
 
-     // notvalidEmail
+  // notvalidEmail
 
-     const res2:any=await request(getHost(),mutation("ma","dd24d4sd4s24ds"));
+  it("miss length mail", async () => {
+    const res2: any = await request(
+      getHost(),
+      mutation("ma", "dd24d4sd4s24ds")
+    );
 
-     expect(res2.register.data).toHaveLength(1);
+    expect(res2.register.data).toHaveLength(1);
+    expect(res2.register.data[0].path).toEqual({
+      path: "email",
+      message: ERROR_MESSAGE_EMAIL.EMAIL_NOT_LONG_ENOUGH,
+    });
+  });
 
+  it("duplication mail and  bad password", async () => {
+    const res2: any = await request(getHost(), mutation("test@test.com", "11"));
 
-     expect(res2.register.data[0].path).toEqual({
-          path:"email",
-          message:ERROR_MESSAGE_EMAIL.EMAIL_NOT_LONG_ENOUGH
+    // expect(res2.register.data).toHaveLength(1);
+    expect(res2.register.data[0]).toEqual([
+      {
+        path: "email",
+        message: ERROR_MESSAGE_EMAIL.DUPLICATION_ERROR,
+      },
+      {
+        path: "password",
+        message: ERROR_MESSAGE_PASSWORD.PASSWORD_NOT_LONG_ENOUGH,
+      },
+    ]);
+  });
+  it(" mail is not long and  fine password", async () => {
+    const res3: any = await request(getHost(), mutation("com", "ddcdcdfdf"));
 
-     });
-     expect(res2.register.data[0].message).toEqual(ERROR_MESSAGE_EMAIL.DUPLICATION_ERROR);
-
-///not mail
-
-
-
-// not main and max
-
-
-// not empty
-
-
-
-
-
-     // expect(res2.register).toEqual({
-     //      register:[{
-     //        path:'email',
-     //        message:ERROR_MESSAGE_EMAIL.DUPLICATION_ERROR
-     //      }]
-     // })
-
-
-
-})
-
+    // expect(res2.register.data).toHaveLength(1);
+    expect(res3.register.data[0]).toEqual([
+      {
+        path: "email",
+        message: ERROR_MESSAGE_EMAIL.EMAIL_NOT_LONG_ENOUGH,
+      },
+    ]);
+  });
+});
--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,10 +1,14 @@
-import * as bcrypt from 'bcryptjs'
-import { User } from 'src/entity/user';
-import { ResolversMap } from './../../types/graphql-utils.d';
-import { MessageError } from './../../utils/MessageError';
-import { ERROR_MESSAGE_EMAIL, ERROR_MESSAGE_PASSWORD } from '../user/Register/ErrorMessages';
-import * as yup from 'yup';
-import { formatYupError } from 'src/utils/YupValidation';
+import * as bcrypt from "bcryptjs";
+import { User } from "src/entity/user";
+import { ResolversMap } from "./../../types/graphql-utils.d";
+import { MessageError } from "./../../utils/MessageError";
+import {
+  ERROR_MESSAGE_EMAIL,
+  ERROR_MESSAGE_PASSWORD,
+} from "../user/Register/ErrorMessages";
+import * as yup from "yup";
+
+import { formatYupError } from "src/utils/YupValidation";
 
 // interface InputArgsRegister{
 //      data:{
@@ -14,50 +18,56 @@ import { formatYupError } from 'src/utils/YupValidation';
 
 // }
 
-const schemaValidationRegister=yup.
-object().
-shape({
-     email:yup.string().email(ERROR_MESSAGE_EMAIL.FORMAT_SHOULD_BE_MAIL).min(7,ERROR_MESSAGE_EMAIL.EMAIL_NOT_LONG_ENOUGH).max(255,ERROR_MESSAGE_EMAIL.EMAIL_NOT_LONG_ENOUGH).required(),
-     password:yup.string().min(6,ERROR_MESSAGE_PASSWORD.PASSWORD_NOT_LONG_ENOUGH).max(30,ERROR_MESSAGE_PASSWORD.PASSWORD_NOT_LONG_ENOUGH).required(),
+const schemaValidationRegister = yup.object().shape({
+  email: yup
+    .string()
+    .email(ERROR_MESSAGE_EMAIL.FORMAT_SHOULD_BE_MAIL)
+    .min(7, ERROR_MESSAGE_EMAIL.EMAIL_NOT_LONG_ENOUGH)
+    .max(255, ERROR_MESSAGE_EMAIL.EMAIL_NOT_LONG_ENOUGH)
+    .required(),
+  password: yup
+    .string()
+    .min(6, ERROR_MESSAGE_PASSWORD.PASSWORD_NOT_LONG_ENOUGH)
+    .max(30, ERROR_MESSAGE_PASSWORD.PASSWORD_NOT_LONG_ENOUGH)
+    .required(),
+});
+export const resolvers: ResolversMap = {
+  Query: {
+    by: () => "bay🙌",
+  },
 
+  Mutation: {
+    register: async (_: any, args: GQL.IRegisterOnMutationArguments) => {
+            let { email, password }:any= args.data;
+      email = email.toLowerCase();
 
-})
-export const resolvers:ResolversMap = {
+      try {
+        await schemaValidationRegister.validate(
+          { email, password },
+          { abortEarly: false }
+        );
+      } catch (error) {
+        return formatYupError(error);
+      }
 
-    Query:{
+      //  check if mail is found
+      const isMail = await User.findOne({ where: { email }, select: ["id"] });
+      if (!isMail) {
+        return [
+          {
+            path: "email",
+            messages: ERROR_MESSAGE_EMAIL.DUPLICATION_ERROR,
+          },
+        ];
+      }
 
-by:()=>"bay🙌"
+      const hashPassword = await bcrypt.hash(password, 20);
+      await User.create({
+        email,
+        password: hashPassword,
+      }).save();
+
+      return null;
     },
-
-     Mutation: {
-          register: async (_:any,{ email, password }:any) => {
-           email=email.toLowerCase();
-
-               try {
-                    await schemaValidationRegister.validate({email,password},{abortEarly:false})}
-                    catch (error) {
-                         return formatYupError(error)
-
-               }
-
-                   //  check if mail is found
-                   const isMail=await User.findOne({where:{email},select:['id']});
-                   if(!isMail){
-                            return[ {
-                                  path:"email",
-                                messages: ERROR_MESSAGE_EMAIL.DUPLICATION_ERROR      }
-                   ];}
-
-                    const hashPassword  = await bcrypt.hash(password,20);
-                   await User.create({
-                        email,
-                        password:hashPassword
-                   }).save();
-
-
-                   return null
-
-          }
-
-     }
-}
+  },
+};
